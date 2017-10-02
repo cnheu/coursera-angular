fb7f7e00b69f94c137208f74975171426be45214
cnheu <tofa.nheu@gmail.com>
2017-10-02 11:56:55 +1100

    Components Part 1

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material';
 
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +19,7 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpModule,
     FlexLayoutModule,
     MatToolbarModule,
 
