408eb723da821c499aead533e63dabe0e5f9609d
cnheu <tofa.nheu@gmail.com>
2017-10-17 00:27:53 +1100

    Added dish service functions and updated DISHES sample model

--- Angular/conFusion/src/app/services/dish.service.ts
+++ Angular/conFusion/src/app/services/dish.service.ts
@@ -13,4 +13,16 @@ export class DishService {
     return DISHES;
   }
 
+  // Get Dish based on ID
+  getDish(id: number): Dish {
+    // shorthand for function using arrow function
+    // select 0th index since DISHES is a list
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  // Get Dish based on featured flag
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => (dish.featured))[0];
+  }
+
 }
