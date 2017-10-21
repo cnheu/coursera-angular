48422aea6a517d1de9dd5fb91089044d040889c4
cnheu <tofa.nheu@gmail.com>
2017-10-21 20:25:32 +1100

    Single Page Applications Part 2

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,14 +12,28 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
   // Declare dish CONST
-  @Input()
+  // Remove @Input, cos it's no longer an input but going to be done
+  // through the router
+  // @Input()
   dish: Dish;
   // Declare comments list
   // comments = this.dish.comments;
 
-  constructor() { }
+  // dish value is no longer available from the dish component
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    // fetch the id from the route parameters (i.e. the url)
+    // the route will be /dishdetail/X
+    let id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    // location service has a back function to use browser history
+    this.location.back();
   }
 
 }
